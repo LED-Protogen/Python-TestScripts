9c6fc4de167f4a1b115aca1e423b2cab0db438b3
LED The Protogen <119714790+LED-Protogen@users.noreply.github.com>
2022-12-11 00:49:03 -0600

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,14 @@
+# This just says hello welcome
+print("Hello, welcome to Furry Cafe UwU!~")
+#This asks for your name
+name = input("What is your name?\n")
+
+print("Hello " + name + ", enjoy your stay~\n\n\n")
+# This tells us are menu
+menu = "Coffee, Pizza, Cake, Latte, Espresso, RAM (For protogens) Cookie"
+
+print(name + ", what would you like for today from our menu? Here is what it is on the menu for today!\n" + menu + "\n\n\n")
+# This will output what we ordered!
+order = input()
+
+print("Sounds good " + name + ", we'ww havw that " + order + " rwady hwrw soon UWU!~")
